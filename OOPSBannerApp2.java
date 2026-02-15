c9be5c0e88c59279edbfd2f895ac0fd28c95bae4
sg7312-svg <sg7312@srmist.edu.in>
2026-02-15 21:23:50 +0530

    Add files via upload

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,24 @@
+
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * Displays the word "OOPS" in ASCII banner format
+ * using asterisks (*) and spaces.
+ *
+ * @author Saksham
+ * @version 2.0
+ */
+
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****   " + "  *****   " + "******  " + "  ******  ");
+        System.out.println(" **   **  " + " **   **  " + "**   ** " + " **       ");
+        System.out.println("**     ** " + "**     ** " + "**   ** " + "**       ");
+        System.out.println("**     ** " + "**     ** " + "******    " +"*****    ");
+        System.out.println("**     ** " + "**     ** " + "**       " + "    **  ");
+        System.out.println(" **   **  " + " **   **  " + "**       " +"**   **  ");
+        System.out.println("  *****   " + "  *****   " + "**       " + " *****   ");
+    }
+}
